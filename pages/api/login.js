bc7152cf00b298055c032e074d0ced16d98a9c6e
Abzal Orazbek <abzalorazbek2003@gmail.com>
2024-01-21 14:43:49 +0600

    Fix login and register endpoints

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import { users } from "./mongo";
-import { NextResponse } from "next/server";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     var email = req.body.email;
@@ -9,18 +8,15 @@ export default async function handler(req, res) {
       res.status(400).json({ text: "Email and password are required!" });
       return;
     }
-    password = await bcrypt.hash(password, 10);
-    users.findOne({ email: email, password: password }, function (err, user) {
-      if (err) {
-        console.err(err);
-        res.status(500).json({ text: "Internal error!" });
-      }
-      if (user) {
-        res.status(200).json({ text: "Login successful!" });
-      } else {
-        res.status(400).json({ text: "Email or password is incorrect!" });
-      }
-    });
+    const user = await users.findOne({ email: email});
+    if(user ==null){
+      res.status(400).json({ text: "User does not exist" });
+    }
+    if(bcrypt.compareSync(password, user.password)){
+      res.status(200).json({ text: "Login successful!" });
+    }else{
+      res.status(400).json({ text: "Invalid password!" });
+    }
   } else {
     res.status(400).json({ text: "Only POST method is allowed!" });
   }
--- pages/api/register.js
+++ pages/api/register.js
@@ -6,28 +6,16 @@ export default async function handler(req, res) {
     var password = req.body.password;
     if (email === "" || password === "") {
       res.status(400).json({ text: "Email and password are required!" });
+      return;
     }
     password = await bcrypt.hash(password, 10);
-    users.findOne({ email: email }, function (err, user) {
-      if (err) {
-        console.err(err);
-        res.status(500).json({ text: "Internal error!" });
-      }
-      if (user) {
-        res.status(400).json({ text: "User already exists" });
-      } else {
-        users.insertOne(
-          { email: email, password: password },
-          function (err, user) {
-            if (err) {
-              console.err(err);
-              res.status(500).json({ text: "Internal error!" });
-            }
-            res.status(200).json({ text: "Registration successful!" });
-          }
-        );
-      }
-    });
+    const user = await users.findOne({ email: email});
+    if(user === null) {
+      users.insertOne({ email: email, password: password });
+      res.status(200).json({ text: "Registration successful!" });
+    }else{
+      res.status(400).json({ text: "Email already exists!" });
+    }
   } else {
     res.status(400).json({ text: "Only POST method is allowed!" });
   }
